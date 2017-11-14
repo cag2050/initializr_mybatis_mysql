fc31b4e514e2709b4bc026d34b6559d238188229
cag2050 <cag2050@163.com>
2017-11-14 18:29:48 +0800

    m

--- src/main/java/com/cag/java/InitializrMybatisMysqlApplication.java
+++ src/main/java/com/cag/java/InitializrMybatisMysqlApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class InitializrMybatisMysqlApplication {
 
     public static void main(String[] args) {
+        System.out.println("test mybatis===");
         SpringApplication.run(InitializrMybatisMysqlApplication.class, args);
     }
 }
