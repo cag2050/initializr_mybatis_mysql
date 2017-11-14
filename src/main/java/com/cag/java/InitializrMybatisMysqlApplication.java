200aa740756a057376a9fd8b5c452f8c9c5a574f
cag2050 <cag2050@163.com>
2017-11-14 16:35:02 +0800

    m

---
+++ src/main/java/com/cag/java/InitializrMybatisMysqlApplication.java
@@ -0,0 +1,12 @@
+package com.cag.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InitializrMybatisMysqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InitializrMybatisMysqlApplication.class, args);
+    }
+}
